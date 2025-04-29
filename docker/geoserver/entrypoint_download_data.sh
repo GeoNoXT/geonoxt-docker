9001254741902b623f16834453d755e779d75021
Niji <mathereall@gmail.com>
2025-04-29 00:16:40 -0600

    add entrypoint_download_data.sh to handle data download initialization and logging

---
+++ docker/geoserver/entrypoint_download_data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# Cargar variables de entorno
+source /root/.bashrc
+
+INVOKE_LOG_STDOUT=${INVOKE_LOG_STDOUT:-TRUE}
+invoke () {
+    if [ $INVOKE_LOG_STDOUT = 'true' ] || [ $INVOKE_LOG_STDOUT = 'True' ]
+    then
+        /usr/local/bin/invoke $@
+    else
+        /usr/local/bin/invoke $@ > /usr/src/geonode/invoke.log 2>&1
+    fi
+    echo "$@ tasks done"
+}
+
+# Verificar si se debe forzar la reinicialización o si no existe el archivo de bloqueo
+if [ "${FORCE_REINIT}" = "true" ] || [ "${FORCE_REINIT}" = "True" ] || [ ! -e "${GEOSERVER_DATA_DIR}/geoserver_init.lock" ]; then
+    echo "Forzando reinicialización o no se encontró geoserver_init.lock."
+    echo "Ejecutando invoke download-data"
+    invoke download-data
+else
+    echo "Los datos ya están inicializados y geoserver_init.lock está presente."
+fi
+
+# Finalizar el script
+echo "Ejecución del entrypoint completada."
